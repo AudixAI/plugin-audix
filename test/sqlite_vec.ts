838ff0ff6450a9fd893e8f02f31eebe9a1e970b3
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2024-12-20 01:55:48 -0500

    docs: Add JSDoc documentation to sqlite_vec.ts

--- test/sqlite_vec.ts
+++ test/sqlite_vec.ts
@@ -14,6 +14,12 @@ function has_jsdoc(x: number | string): number | string {
 }
 
 
+/**
+ * A function that takes a number or a string as input and return a number if the input is a number or return the input value converted to uppercase if it is a string.
+ * 
+ * @param {number | string} x - The input value which can be a number or a string.
+ * @returns {number | string} - The value multiplied by 2 if it is a number, or converted to uppercase if it is a string.
+ */
 function does_not_have_jsdoc(x: number | string): number | string {
     if (typeof x === 'number') {
         return x * 2;
@@ -35,35 +41,74 @@ function does_not_have_jsdoc_2(x: number | string): number | string {
     return x.toUpperCase();
 }
 
+/**
+* Class representing a simple class.
+*/
+
+/**
+ * Class representing SimpleClass.
+ */
+ */
 
 class SimpleClass {
 
-    /**
-     * Constructor for the class with a specified x value.
-     * @param {number} x - The value of x to be initialized with.
-     */
-    constructor(public x: number) { }
+/**
+ * Constructor for the class with a specified x value.
+/**
+ * Constructor for initializing the value of x.
+ * @param x - The number value to initialize x with.
+ */
 
-    /**
-     * Multiply a number by 2.
-     *
-     * @param {number} x - The number to be multiplied.
-     * @returns {number} The result of multiplying the input number by 2.
-     */
+ * @param {number} x - The value of x to be initialized with.
+ */
+    constructor(public x: number) { }
+/**
+ * Multiplies the input number by 2.
+ * @param {number} x - The number to be multiplied.
+ * @returns {number} The result of multiplying the input number by 2.
+ */
+
+
+/**
+ * Multiply a number by 2.
+ *
+/**
+ * Converts a number to a string.
+ * 
+ * @param {number} x - The number to be converted to a string.
+ * @returns {string} The number converted to a string.
+ */
+
+ * @param {number} x - The number to be multiplied.
+ * @returns {number} The result of multiplying the input number by 2.
+ */
+/**
+ * Converts a number to a string.
+ * 
+ * @param x The number to convert.
+ * @returns The string representation of the input number.
+ */
+
     simpleMethod(x: number): number {
         return x * 2;
     }
 
-    /**
-     * Convert a number to a string.
-     * 
-     * @param {number} x - The number to be converted to a string.
-     * @returns {string} The string representation of the input number.
-     */
+/**
+ * Convert a number to a string.
+ * 
+ * @param {number} x - The number to be converted to a string.
+ * @returns {string} The string representation of the input number.
+ */
     simpleMethod2(x: number): string {
         return x.toString();
     }
 
+/**
+ * Converts a number to a string.
+ * 
+ * @param {number} x - The number to be converted to a string.
+ * @returns {string} A string representation of the input number.
+ */
     simpleMethod3(x: number): string {
         return x.toString();
     }
