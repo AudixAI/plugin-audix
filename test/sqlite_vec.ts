5dab861b88ee75138b2efe87dc14b8c300d30d54
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2024-12-20 01:20:07 -0500

    docs: Add JSDoc documentation to sqlite_vec.ts

--- test/sqlite_vec.ts
+++ test/sqlite_vec.ts
@@ -1,3 +1,9 @@
+/**
+ * Takes a number or string as input and either doubles the number if it is a number or converts the string to uppercase if it is a string.
+ * 
+ * @param {number | string} x - The input number or string
+ * @returns {number | string} The result of the operation based on the type of input
+ */
 function has_jsdoc(x: number | string): number | string {
     if (typeof x === 'number') {
         return x * 2;
@@ -6,6 +12,13 @@ function has_jsdoc(x: number | string): number | string {
 }
 
 
+/**
+ * This function takes in a number or a string as input and performs different operations based on the type:
+ * - If the input is a number, it multiplies the number by 2 and returns the result.
+ * - If the input is a string, it converts the string to uppercase and returns it.
+ * @param {number | string} x - The input number or string
+ * @returns {number | string} - The result of the operation
+ */
 function does_not_have_jsdoc(x: number | string): number | string {
     if (typeof x === 'number') {
         return x * 2;
@@ -13,6 +26,12 @@ function does_not_have_jsdoc(x: number | string): number | string {
     return x.toUpperCase();
 }
 
+/**
+ * Takes a number or a string and either doubles the number
+ * or returns the uppercase version of the string.
+ * @param {number | string} x - The input value to be processed.
+ * @returns {number | string} The processed value.
+ */
 function does_not_have_jsdoc_2(x: number | string): number | string {
     if (typeof x === 'number') {
         return x * 2;
@@ -20,6 +39,10 @@ function does_not_have_jsdoc_2(x: number | string): number | string {
     return x.toUpperCase();
 }
 
+/**
+ * Class representing a SimpleClass.
+ */
+ */
 class SimpleClass {
 
     constructor(public x: number) { }
