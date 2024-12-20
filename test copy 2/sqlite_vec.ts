9007239e335bf551b06e58af542eeae2cc799a19
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2024-12-20 02:10:08 -0500

    docs: Add JSDoc documentation to sqlite_vec.ts

--- test copy 2/sqlite_vec.ts
+++ test copy 2/sqlite_vec.ts
@@ -1,4 +1,11 @@
 
+/**
+ * Takes a number or string as input and performs a specific operation based on the type.
+ * If the input is a number, it doubles the number value.
+ * If the input is a string, it converts the string to uppercase.
+ * @param {number | string} x - The input number or string value.
+ * @returns {number | string} - The result of the operation, which can be a number or string.
+ */
 function has_jsdoc(x: number | string): number | string {
     if (typeof x === 'number') {
         return x * 2;
@@ -35,14 +42,35 @@ function does_not_have_jsdoc_2(x: number | string): number | string {
 }
 
 
+/**
+ * Convert a number to a string.
+ * 
+ * @param {number} x - The number to be converted to a string.
+ * @returns {string} The string representation of the input number.
+ */
 class SimpleClass {
 
+/**
+ * Constructor for creating an instance of a class with a specified x value.
+ * @param {number} x - The value for the x property of the instance.
+ */
     constructor(public x: number) { }
 
+/**
+ * Multiplies the input number by 2.
+ * @param {number} x - The number to be multiplied
+ * @returns {number} The result of the multiplication
+ */
     simpleMethod(x: number): number {
         return x * 2;
     }
 
+/**
+ * Converts a number to a string.
+ *
+ * @param {number} x - The number to convert to a string.
+ * @returns {string} The string representation of the input number.
+ */
     simpleMethod2(x: number): string {
         return x.toString();
     }
