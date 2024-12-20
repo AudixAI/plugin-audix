ef25fd1bbf48dd6bbcf1122f0f8af3d56fac15ba
Ed-Marcavage <edmarcavage256@proton.me>
2024-12-20 02:02:53 -0500

    Stress

--- test copy/sqlite_vec.ts
+++ test copy/sqlite_vec.ts
@@ -14,12 +14,7 @@ function has_jsdoc(x: number | string): number | string {
 }
 
 
-/**
- * A function that takes a number or a string as input and return a number if the input is a number or return the input value converted to uppercase if it is a string.
- * 
- * @param {number | string} x - The input value which can be a number or a string.
- * @returns {number | string} - The value multiplied by 2 if it is a number, or converted to uppercase if it is a string.
- */
+
 function does_not_have_jsdoc(x: number | string): number | string {
     if (typeof x === 'number') {
         return x * 2;
@@ -47,38 +42,38 @@ function does_not_have_jsdoc_2(x: number | string): number | string {
 
 class SimpleClass {
 
-/**
- * Constructor for the class with a specified x value.
- * @param {number} x - The value of x to be initialized with.
- */
+    /**
+     * Constructor for the class with a specified x value.
+     * @param {number} x - The value of x to be initialized with.
+     */
     constructor(public x: number) { }
 
-/**
- * Multiply a number by 2.
- *
- * @param {number} x - The number to be multiplied.
- * @returns {number} The result of multiplying the input number by 2.
- */
+    /**
+     * Multiply a number by 2.
+     *
+     * @param {number} x - The number to be multiplied.
+     * @returns {number} The result of multiplying the input number by 2.
+     */
     simpleMethod(x: number): number {
         return x * 2;
     }
 
-/**
- * Convert a number to a string.
- * 
- * @param {number} x - The number to be converted to a string.
- * @returns {string} The string representation of the input number.
- */
+    /**
+     * Convert a number to a string.
+     * 
+     * @param {number} x - The number to be converted to a string.
+     * @returns {string} The string representation of the input number.
+     */
     simpleMethod2(x: number): string {
         return x.toString();
     }
 
-/**
- * Converts a number to a string.
- * 
- * @param {number} x - The number to be converted to a string.
- * @returns {string} A string representation of the input number.
- */
+    /**
+     * Converts a number to a string.
+     * 
+     * @param {number} x - The number to be converted to a string.
+     * @returns {string} A string representation of the input number.
+     */
     simpleMethod3(x: number): string {
         return x.toString();
     }
