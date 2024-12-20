b53c42826f6261f03f5bfdcbc900111f426a09e9
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2024-12-20 02:04:15 -0500

    docs: Add JSDoc documentation to sqlite_vec.ts

--- test copy 2/sqlite_vec.ts
+++ test copy 2/sqlite_vec.ts
@@ -1,4 +1,10 @@
 
+/**
+ * Returns the value multiplied by 2 if it's a number, otherwise returns the value in upper case.
+ * 
+ * @param {number | string} x - The input value to be processed.
+ * @returns {number | string} The processed output value.
+ */
 function has_jsdoc(x: number | string): number | string {
     if (typeof x === 'number') {
         return x * 2;
@@ -35,14 +41,36 @@ function does_not_have_jsdoc_2(x: number | string): number | string {
 }
 
 
+/**
+ * Convert a number to a string.
+ * 
+ * @param {number} x - The number to be converted to a string.
+ * @returns {string} The string representation of the input number.
+ */
+      
 class SimpleClass {
 
+/**
+ * Constructor for creating an instance of the class with the provided value for 'x'.
+ * @param {number} x - The value for 'x' property.
+ */
     constructor(public x: number) { }
 
+/**
+ * Multiplies a number by 2.
+ * @param {number} x - The number to be multiplied
+ * @returns {number} The result of multiplying the input number by 2
+ */
     simpleMethod(x: number): number {
         return x * 2;
     }
 
+/**
+ * Converts a number to a string.
+ * 
+ * @param {number} x - The number to be converted to a string
+ * @returns {string} The number converted to a string
+ */
     simpleMethod2(x: number): string {
         return x.toString();
     }
