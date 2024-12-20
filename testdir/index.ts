02fa9b5ef6c1bd53c11478c4d3ded4b35122ef91
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2024-12-19 23:14:09 -0500

    docs: Add JSDoc documentation to index.ts

--- testdir/index.ts
+++ testdir/index.ts
@@ -1,3 +1,11 @@
+/**
+ * /**
+ *  * This function takes a number or string as input and returns either the result of doubling the number or converting the string to uppercase.
+ *  * 
+ *  * @param {number | string} x - The input number or string
+ *  * @returns {number | string} - The result of doubling the number or converting the string to uppercase
+ *  */
+ */
 function test_pr(x: number | string): number | string {
     if (typeof x === 'number') {
         return x * 2;
