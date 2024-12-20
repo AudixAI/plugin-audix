a7c5f9b070a19880595e4ab6563a600140497a65
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2024-12-20 01:41:31 -0500

    docs: Add JSDoc documentation to sqlite_vec.ts

--- test/sqlite_vec.ts
+++ test/sqlite_vec.ts
@@ -1,3 +1,11 @@
+/**
+ * This function takes a number or string as input and returns a modified value based on the type of the input.
+ * If the input is a number, it will return double the value of the number.
+ * If the input is a string, it will return the uppercase version of the string.
+ *
+ * @param {number | string} x - The input value that can be a number or a string.
+ * @returns {number | string} The modified value based on the type of the input.
+ */
 function has_jsdoc(x: number | string): number | string {
     if (typeof x === 'number') {
         return x * 2;
@@ -6,6 +14,13 @@ function has_jsdoc(x: number | string): number | string {
 }
 
 
+/**
+ * Takes a number or string as input and returns either the doubled number
+ * or the input string converted to uppercase.
+ * 
+ * @param {number | string} x - The input number or string
+ * @returns {number | string} - The doubled number or the input string in uppercase
+ */
 function does_not_have_jsdoc(x: number | string): number | string {
     if (typeof x === 'number') {
         return x * 2;
@@ -13,6 +28,11 @@ function does_not_have_jsdoc(x: number | string): number | string {
     return x.toUpperCase();
 }
 
+/**
+ * Takes a number or string as input and returns either the number multiplied by 2 or the string converted to uppercase.
+ * @param {number | string} x - The input number or string
+ * @returns {number | string} Either the number multiplied by 2 or the string converted to uppercase
+ */
 function does_not_have_jsdoc_2(x: number | string): number | string {
     if (typeof x === 'number') {
         return x * 2;
@@ -20,6 +40,10 @@ function does_not_have_jsdoc_2(x: number | string): number | string {
     return x.toUpperCase();
 }
 
+/**
+ * Represents a simple class.
+ */
+       
 class SimpleClass {
 
     constructor(public x: number) { }
