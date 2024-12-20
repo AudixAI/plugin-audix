2a12a19292008ed6632074c45350b5697e12c816
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2024-12-20 02:04:15 -0500

    docs: Add JSDoc documentation to sqlite_vec.ts

--- test copy/sqlite_vec.ts
+++ test copy/sqlite_vec.ts
@@ -15,6 +15,13 @@ function has_jsdoc(x: number | string): number | string {
 
 
 
+/**
+ * Takes a number or string as input and returns either the input multiplied by 2 
+ * if it is a number, or the input converted to uppercase if it is a string.
+ * 
+ * @param {number | string} x - The input number or string.
+ * @returns {number | string} - The result of the operation based on the input type.
+ */
 function does_not_have_jsdoc(x: number | string): number | string {
     if (typeof x === 'number') {
         return x * 2;
@@ -40,6 +47,32 @@ function does_not_have_jsdoc_2(x: number | string): number | string {
 * Class representing a simple class.
 */
 
+/**
+ * Constructor for the class with a specified x value.
+ * @param {number} x - The value of x to be initialized with.
+ */
+
+/**
+ * Multiply a number by 2.
+ *
+ * @param {number} x - The number to be multiplied.
+ * @returns {number} The result of multiplying the input number by 2.
+ */
+
+/**
+ * Convert a number to a string.
+ * 
+ * @param {number} x - The number to be converted to a string.
+ * @returns {string} The string representation of the input number.
+ */
+
+/**
+ * Converts a number to a string.
+ * 
+ * @param {number} x - The number to be converted to a string.
+ * @returns {string} A string representation of the input number.
+ */
+ * /
 class SimpleClass {
 
     /**
