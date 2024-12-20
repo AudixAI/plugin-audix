d29b4fdc4b34c0909ce5d31ef67ae448ff8bd70b
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2024-12-19 22:25:13 -0500

    Add JSDoc documentation to test_create_pr2.ts

--- test_create_pr2.ts
+++ test_create_pr2.ts
@@ -17,6 +17,14 @@ function test_pr(x: number | string): number | string {
     return x.toUpperCase();
 }
 
+/**
+ * /**
+ * * Takes a number or string as input and returns either the input multiplied by 2 if it is a number,
+ * * or the input converted to uppercase if it is a string.
+ * * @param {number | string} x - The input number or string.
+ * * @return {number | string} - The result of the operation depending on the type of the input.
+ * */
+ */
 function test_pr2(x: number | string): number | string {
     if (typeof x === 'number') {
         return x * 2;
