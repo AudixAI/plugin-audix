2428e171fa7364f5e3cff1fe658a304f0ded2420
Ed Marcavage <61299527+Ed-Marcavage@users.noreply.github.com>
2024-12-19 23:25:14 -0500

    docs: Add JSDoc documentation to sqlite_vec.ts

--- test/sqlite_vec.ts
+++ test/sqlite_vec.ts
@@ -1,7 +1,47 @@
+/**
+ * Takes a number or a string and returns either the number multiplied by 2
+ * if the input is a number, or the input string converted to uppercase if it is a string.
+ * @param {number | string} x - The input value that can be a number or a string.
+ * @returns {number | string} The output value which is either the number multiplied by 2 or the input string converted to uppercase.
+ */
 function has_jsdoc(x: number | string): number | string {
     if (typeof x === 'number') {
+/**
+ * A function that either doubles a number or converts a string to uppercase.
+ * @param {number | string} x The input value that can be either a number or a string
+ * @returns {number | string} The output value that is either the doubled number (if input is a number) or the input string converted to uppercase
+ */
         return x * 2;
     }
+/**
+ * A function that takes in a number or string as input and performs different operations based on the type.
+ * If the input is a number, it returns the input multiplied by 2.
+ * If the input is a string, it returns the input converted to uppercase.
+ * @param {number | string} x - The input value to be processed.
+ * @returns {number | string} The processed value.
+ */
+/**
+ * Constructor for creating an instance of the class with a specified 'x' value.
+ * @param {number} x - The value of 'x' to initialize the instance with.
+ */
+/**
+ * A simple method that multiplies the input number by 2.
+ * 
+ * @param {number} x - The input number to be multiplied.
+ * @returns {number} The result of multiplying the input number by 2.
+ */
+/**
+ * Converts a number to a string representation.
+ * 
+ * @param x - The number to convert to a string.
+ * @returns A string representation of the input number.
+ */
+/**
+ * Takes a number as input and returns the number converted to a string.
+ * 
+ * @param {number} x - The number to be converted to a string.
+ * @returns {string} The number converted to a string.
+ */
     return x.toUpperCase();
 }
 
